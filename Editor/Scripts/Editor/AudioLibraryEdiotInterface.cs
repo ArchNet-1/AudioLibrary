a32686f485827b267a29d147bb92ef0301d6a828
Louis <lpakel@tilakhealthcare.com>
2023-11-08 19:54:53 +0100

    Update version 1.0.1
    Clean code, reformat the library to use one specific enum

---
+++ Editor/Scripts/Editor/AudioLibraryEdiotInterface.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Library.Editor
+{
+    [CustomEditor(typeof(AudioLibrary))]
+    public class AudioLibraryEditorInterface : LibraryEditorInterface<AudioLibrary, AudioClip>
+    {
+        protected override bool AreDifferent(AudioClip a, AudioClip b) => a != b;
+
+        protected override AudioClip DisplayGuiAndSelect(string label, AudioClip currentValue)
+        {
+            return EditorGUILayout.ObjectField(
+                label,
+                currentValue,
+                typeof(AudioClip),
+                false
+            ) as AudioClip;
+        }
+    }
+}
---
+++ Editor/Scripts/Music.cs
@@ -0,0 +1,16 @@
+﻿namespace Library
+{
+    public enum Music
+    {
+        Awaken,
+        Discovery,
+        LaserQuest,
+        SearchOfDestiny,
+        TheWarAgainst,
+        UnknownWorld,
+        WhisperOfTime,
+        WorldMap,
+        WorldMapWindEffect,
+        LoadingScreen
+    }
+}
